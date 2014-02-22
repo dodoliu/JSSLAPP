2bee8c5768582f058ab71cd2a9088708222b6163
LDY <liudongyue1987@163.com>
2014-02-22 12:18:52 +0800

    Initial Commit

---
+++ JSSL/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JSSL
+//
+//  Created by LDY on 14-2-22.
+//  Copyright (c) 2014年 DONLIU1987. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JSSLTests/JSSLTests.m
@@ -0,0 +1,34 @@
+//
+//  JSSLTests.m
+//  JSSLTests
+//
+//  Created by LDY on 14-2-22.
+//  Copyright (c) 2014年 DONLIU1987. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JSSLTests : XCTestCase
+
+@end
+
+@implementation JSSLTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
